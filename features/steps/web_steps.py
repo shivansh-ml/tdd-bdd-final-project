f3d6d0fd23759edbf8363652e0bddd49f3db2a6f
shivansh-ml <shivanshjha93@gmail.com>
2026-06-20 11:40:47 -0400

    Completed TDD BDD final project

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -105,7 +105,34 @@ def step_impl(context, element_name):
 ##################################################################
 
 ## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element(By.ID, button_id).click()
 
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
+
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert(name in element.text)
+
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert(name not in element.text)
+    
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
